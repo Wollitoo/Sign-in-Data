7c614a6b7979566f91efd71c10962206c74086e1
Wollitoo <wollitoo.waldschrat@gmx.de>
2016-12-20 21:59:52 +0100

    initial release

---
+++ v1.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Wollitoo
+ * Date: 20.12.2016
+ * Time: 19:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Name_Check
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//Process.Start("http://www.line-lan.net");
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			if(firstName.Text.Equals("") || lastName.Text.Equals(""))	{
+			   	MessageBox.Show("Pls fill in all needed fields!");
+			}else{
+			   	if(firstName.Text.Equals("André") && lastName.Text.Equals("Kienzle"))	{
+			   		MessageBox.Show("Happy Birthday! Mach weiter so wie du bist! Du machst ne Hammer Arbeit als Serveradmin, Programmierer und Freund. Ich beneide dich um deine Skills für mehr als das reichts im mom net.");
+			   	}else{
+			   		MessageBox.Show("Wrong User!");
+			   	}
+			}
+		}
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("http://www.line-lan.net");
+		}
+	}
+}
---
+++ v1_MainForm.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Wollitoo
+ * Date: 20.12.2016
+ * Time: 19:10
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Name_Check
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox firstName;
+		private System.Windows.Forms.TextBox lastName;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.firstName = new System.Windows.Forms.TextBox();
+			this.lastName = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(11, 71);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Check!";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 17);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Insert first name:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 19);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Insert last name:";
+			// 
+			// firstName
+			// 
+			this.firstName.Location = new System.Drawing.Point(119, 13);
+			this.firstName.Name = "firstName";
+			this.firstName.Size = new System.Drawing.Size(100, 20);
+			this.firstName.TabIndex = 3;
+			// 
+			// lastName
+			// 
+			this.lastName.Location = new System.Drawing.Point(119, 40);
+			this.lastName.Name = "lastName";
+			this.lastName.Size = new System.Drawing.Size(100, 20);
+			this.lastName.TabIndex = 4;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(119, 71);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(117, 82);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.PictureBox1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(245, 159);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.lastName);
+			this.Controls.Add(this.firstName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Login Thing";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
